7b0522ff9ee62fafd3ae40c3e9449268df4a2ebe
Alain1001 <alainru1001@gmail.com>
2021-10-10 09:27:38 -0700

    added printhex, printint

--- printint.c
+++ printint.c
@@ -71,5 +71,4 @@ int printint(char *format, va_list pa)
                                                                                                                                         
         numro = integer(num);                                                                                                           
         return (numro);                                                                                                                 
-}                                                                                                                                       
-
+}    
